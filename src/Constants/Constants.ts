f1b71fe7b3b958591f5beb55a50a6e729eee5f2a
Matthew Schweitzer <matthew.schweitzer@slalom.com>
2022-05-11 17:06:02 -0400

    Initial commit

---
+++ src/Constants/Constants.ts
@@ -0,0 +1,20 @@
+export enum CodeColours {
+  RED = "red",
+  BLUE = "blue",
+  GREEN = "green",
+  ORANGE = "orange",
+  PURPLE = "purple",
+  YELLOW = "yellow"
+}
+
+export enum HintColours {
+  WHITE = "white",
+  BLACK = "black"
+}
+
+export enum Messages {
+  TOO_MANY_TRIES = "Error: you have had more than 10 tries!",
+  LENGTH_MUST_BE_4 = "Error: you must pass 4 colours!",
+  INVALID_COLOUR = "Error: you have given an invalid colour!",
+  WON = "WON!",
+}
